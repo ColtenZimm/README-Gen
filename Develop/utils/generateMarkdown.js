c109709083ca642fcc179843530e997464d6805a
ColtenZimm <coltenz00@gmail.com>
2023-05-18 18:17:48 -0500

    maybe fixed pathing

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -62,16 +62,63 @@ function renderLicenseLink(license) {
     case "The Unlicense":
       badge = { name: "The+Unlicense", color: "blueviolet" };
       break;
+  }
 }
-
+return `https://img.shields.io/static/v1?label=license&message=${badge.name}&color=${badge.color})`;
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let path;
+  
+  switch (license) {
+    case "GNU AGPLv3":
+      path = "agpl-3.0";
+      break;
+    case "GNU GPLv3":
+      path = "gpl-3.0";
+      break;
+    case "GNU LGPLv3":
+      path = "lgpl-3.0";
+      break;
+    case "Mozilla Public License 2.0":
+      path = "mpl-2.0";
+      break;
+    case "Apache License 2.0":
+      path = "apache-2.0";
+      break;
+    case "MIT License":
+      path = "mit";
+      break;
+    case "Boost Software License 1.0":
+      path = "bsl-1.0";
+      break;
+    case "The Unlicense":
+      path = "unlicense";
+      break;
+  }
+  return `https://choosealicense.com/licenses/${path}/`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  readme = f"# {title}\n\n"
+  readme += f"{description}\n\n"
+  readme += "## Table of Contents\n\n"
+  readme += "- [Installation](#installation)\n"
+  readme += "- [Usage](#usage)\n"
+  readme += "- [Contributors](#contributors)\n"
+  readme += "- [License](#license)\n\n"
+  readme += "## Installation\n\n"
+  readme += f"{installation}\n\n"
+  readme += "## Usage\n\n"
+  readme += f"{usage}\n\n"
+  readme += "## Contributors\n\n"
+  readme += f"{contributors}\n\n"
+  readme += "## License\n\n"
+  readme += f"This project is licensed under the {license} license.\n"
 
+  return readme
 `;
 }
 
