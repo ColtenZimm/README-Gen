78b20f418f82f051aa1dd3f60eab8142cfd3859c
ColtenZimm <coltenz00@gmail.com>
2023-05-18 18:07:34 -0500

    commit's were in wrong section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  var licenseBadge = generateBadge(userResponse.license)
+  var licenseUrl = generateLicenseUrl(userResponse.license)
+  var result = (`# ${userResponse.title}
+  \n![License](${licenseBadge} \n
+    ${userResponse.description}
+    \n## Table of Contents
+    \n* [Installation](#Installation)
+    \n* [Instructions](#Instructions)
+    \n* [Contributors](#Contributors)
+    \n* [Tests](#Tests)
+    \n* [License](#License)
+    \n* [Author](#Author)
+    \n## Installation
+    \n${userResponse.installation}
+    \n## Usage
+    \n${userResponse.usage}
+    \n## Contributing
+    \n${userResponse.contributing}
+    \n## Tests
+    \n${userResponse.tests}
+    \n## License
+    \nThis project is licensed under the ${userResponse.license} - see the [License](${licenseUrl}) page.
+    \n## Author
+    \n![ProfileImage](${userInfo.avatar_url})
+    \n${userInfo.name}
+    \nLocation: ${userInfo.location}
+    \n[GitHub](${userInfo.html_url})`)
+
+    return result;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
